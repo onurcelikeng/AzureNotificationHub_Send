139ce661e323220768d4a8d236deddef02a5de5e
Onur ÇELİK <Onur ELK>
2017-01-08 02:46:20 +0300

    First commit

---
+++ SendNotification/Helpers/ConfigureHelper.cs
@@ -0,0 +1,9 @@
+﻿namespace SendNotification.Helpers
+{
+    public sealed class ConfigureHelper
+    {
+        public static readonly string HubName = "<your hubname>";
+
+        public static readonly string Connection = "<your connection url>";
+    }
+}
---
+++ SendNotification/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.Azure.NotificationHubs;
+using SendNotification.Helpers;
+
+namespace SendNotification
+{
+    public sealed class Program
+    {
+
+        private static void Main(string[] args)
+        {
+            Console.ReadLine();
+        }
+
+
+        private static async void SendTileNotification(string message)
+        {
+            NotificationHubClient hub = NotificationHubClient.CreateClientFromConnectionString(ConfigureHelper.Connection, ConfigureHelper.HubName);
+
+            string toast = "<?xml version=\"1.0\" encoding=\"utf-8\"?>" +
+                "<tile>" +
+                   "<visual>" +
+                       "<binding template=\"TileSquareText04\">" +
+                          "<text id=\"1\">" + message + "</text>" +
+                       "</binding>" +
+                   "</visual>" +
+                "</tile>";
+
+            await hub.SendWindowsNativeNotificationAsync(toast);
+        }
+
+        private static async void SendToastNotification(string message)
+        {
+            NotificationHubClient hub = NotificationHubClient.CreateClientFromConnectionString(ConfigureHelper.Connection, ConfigureHelper.HubName);
+
+            string toast = "<?xml version=\"1.0\" encoding=\"utf-8\"?>" +
+                "<toast>" +
+                   "<visual>" +
+                       "<binding template=\"ToastText01\">" +
+                          "<text id=\"1\">" + message + "</text>" +
+                       "</binding>" +
+                   "</visual>" +
+                "</toast>";
+
+            await hub.SendWindowsNativeNotificationAsync(toast);
+        }
+    }
+}
